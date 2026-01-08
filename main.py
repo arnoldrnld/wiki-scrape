6d5412667f5f64e28ed7f864d158c29fe376cecc
Arnold Raphel <arnoldrnld101@gmail.com>
2026-01-08 18:00:17 +0530

    Update main: add postgresworker

--- main.py
+++ main.py
@@ -7,28 +7,41 @@ from multiprocessing import Queue
 
 from workers.WikiWorker import WikiWorker
 from workers.YahooFinanceWorkers import YahooFinancePriceScheduler
+from workers.PostgresWorker import PostgresMasterScheduler
 
 
 def main():
     symbol_queue = Queue()
+    postgres_queue = Queue()
     calc_start_time = time.time()
     
     wikiWorker = WikiWorker()
+
     # Track threads
     yahoo_finance_price_scheduler_threads = []
     num_yahoo_finance_price_workers = 4
-
     # Multiple workers run same time
     # As Queue prevent race condition, no two threads work on same symbol
     for i in range(num_yahoo_finance_price_workers):
         # We initialise the thread, input_queue is empty
         # Thread start waiting for value in input_queue
-        yahooFinancePriceScheduler = YahooFinancePriceScheduler(input_queue=symbol_queue)
+        yahooFinancePriceScheduler = YahooFinancePriceScheduler(input_queue=symbol_queue, output_queue=[postgres_queue])
         yahoo_finance_price_scheduler_threads.append(yahooFinancePriceScheduler)
 
-    # symbol_queue gets value , thus input_queue will have value, both points to same queue object
+    postgres_scheduler_threads = []
+    num_postgres_workers = 2
+    for i in range(num_postgres_workers):
+        postgresScheduler = PostgresMasterScheduler(input_queue=postgres_queue)
+        postgres_scheduler_threads.append(postgresScheduler)
+
+
+    symbol_counter = 0
     for symbol in wikiWorker.get_sp_500_companies():
+        # symbol_queue gets value , thus input_queue will have value, both points to same queue object
         symbol_queue.put(symbol)
+        symbol_counter += 1
+        if symbol_counter >= 10:    # Limit for 5 symbol
+            break
 
     # Add 'DONE' for number of threads
     # So each thread will stop gracefully
@@ -38,15 +51,11 @@ def main():
     for thread in yahoo_finance_price_scheduler_threads:
         thread.join()
 
+    for thread in postgres_scheduler_threads:
+        thread.join()
+
     print(f'Time taken: {round(time.time() - calc_start_time, 2)}')
         
     
 if __name__=='__main__':
     main()    
-
-"""Next Steps
-ONE
-- Integrate Postgres
-- Use sqlalchemy
-- Store credentials as environment variables
-- Design it as PostgresWorkers running parallelly with PostgresSchedulers"""
